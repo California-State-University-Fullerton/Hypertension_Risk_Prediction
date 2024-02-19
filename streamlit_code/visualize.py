1359a2dde16061e863535f1b54046ac0efdf46bc
csuftitan <csuftitan@MacBook-Air-307.lan>
2024-02-18 20:03:34 -0800

    Simple push

--- streamlit_code/visualize.py
+++ streamlit_code/visualize.py
@@ -1,3 +1,11 @@
 import streamlit as sl
+import pandas as pd
+import numpy as np
+
 sl.title("Hypertension Risk Prediction")
 sl.subheader("Dashboard")
+
+data = pd.read_csv('../../Assets/heart-cases-location-data.csv', usecols=['longitude','latitude'])
+data.dropna()
+sl.map(data)
+
